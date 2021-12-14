eb6e59eb03e8560bc57cca67bc6ecfed2722f607
Brandi Nichols-Templeton <baustin33@ivytech.edu>
2021-12-14 15:29:13 -0500

    changed to strings

--- models/course.js
+++ models/course.js
@@ -1,12 +1,11 @@
 const db = require("../db");
 
 const Course = db.model("Course",{
-    title: {type:String, required:true},
-    instructor: {type: String},
-    popularity: {type: Number, min:1, max:10},
-    releaseDate: {type: Date, default:Date.now},
-    subject: {type: String}
+    course: {type:String, required:true},
+    instructor: String,
+    subject: String
 
 });
 
 module.exports = Course;
+
